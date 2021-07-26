cdaf4afa1c0b5fb4d730975e0fb70492369fdd90
Fabio Alfineo di Gabriele <fabioa.digabriele@gmail.com>
2021-07-26 23:57:00 +0200

    preparing autentication get APK

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -4,10 +4,16 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import com.udacity.project4.R
-import com.udacity.project4.utils.Constants
+import com.udacity.project4.databinding.ActivityAuthenticationBinding
+import com.udacity.project4.locationreminders.RemindersActivity
 import com.udacity.project4.utils.Constants.AUTENTICATION_CODE
+import com.udacity.project4.utils.Constants.FIREBASE_TAG
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -15,26 +21,25 @@ import com.udacity.project4.utils.Constants.AUTENTICATION_CODE
  */
 class AuthenticationActivity : AppCompatActivity() {
 
+    lateinit var binding : ActivityAuthenticationBinding
+    private val providers = arrayListOf(
+        AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build()
+    )
+    //todo divide this activity in two fragment login/registration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_authentication)
-//         TODO: Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
-        val providers = arrayListOf(
-            AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build()
-        )
+        binding = ActivityAuthenticationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        startActivityForResult(
-            AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(
-                providers
-            ).build(), AUTENTICATION_CODE)
-
-//          TODO: If the  was authenticated, send him to RemindersActivity
-//        if(false){
-//        val intent = Intent(this, RemindersActivity::class.java)
-//        startActivity(intent)}
-
-//          TODO: a bonus is to customize the sign in flow to look nice using :
-        //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
+        binding.buttonLogin.setOnClickListener {
+            // TODO: a bonus is to customize the sign in flow to look nice using :
+            //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
+            startActivityForResult(
+                AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(
+                    providers
+                ).build(), AUTENTICATION_CODE)
+        }
 
     }
 
@@ -43,17 +48,18 @@ class AuthenticationActivity : AppCompatActivity() {
         if (requestCode == AUTENTICATION_CODE) {
             val response = IdpResponse.fromResultIntent(data)
             if (resultCode == Activity.RESULT_OK) {
-                // Successfully signed in user.
+                // Successfully signed in user
                 Log.i(
-                    Constants.TAG,
-                    "Successfully signed in user " +
-                            "${FirebaseAuth.getInstance().currentUser?.displayName}!"
+                    FIREBASE_TAG, "Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!"
                 )
+
+                val intent = Intent(this, RemindersActivity::class.java)
+                startActivity(intent)
             } else {
-                // Sign in failed. If response is null the user canceled the sign-in flow using
-                // the back button. Otherwise check response.getError().getErrorCode() and handle
-                // the error.
-                Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
+                // Sign in failed
+                Log.i(FIREBASE_TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
+
+                Toast.makeText(this, resources.getText(R.string.login_failed), Toast.LENGTH_LONG).show()
             }
         }
     }
--- app/src/main/java/com/udacity/project4/utils/Constants.kt
+++ app/src/main/java/com/udacity/project4/utils/Constants.kt
@@ -4,5 +4,7 @@ object Constants {
 
     const val AUTENTICATION_CODE = 1
     const val TAG = "Ex4LogMessage"
-    const val FIREBASETAG = "Ex4LogFirebaseMessage"
+    const val FIREBASE_TAG = "Ex4LogFirebaseMessage"
+
+    //todo  aggiungi a local.proprieties MAPS_API_KEY=AIzaSyAVu8CO-7qWOXG-SHxueMnFMjZns5ifXeM
 }
