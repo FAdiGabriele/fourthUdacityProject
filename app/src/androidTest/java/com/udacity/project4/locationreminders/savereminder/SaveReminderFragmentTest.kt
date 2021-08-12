f91ed10dd83d0f7d73c7f9a71b12efc1c0819224
Fabio Alfineo di Gabriele <fabioa.digabriele@gmail.com>
2021-08-13 01:50:33 +0200

    test saveReminderFragment

--- app/src/androidTest/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragmentTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragmentTest.kt
@@ -1,31 +1,36 @@
 package com.udacity.project4.locationreminders.savereminder
 
+import android.app.Application
 import android.os.Bundle
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.test.espresso.Espresso
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.replaceText
-import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.RootMatchers.withDecorView
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import androidx.test.rule.ActivityTestRule
 import com.udacity.project4.R
+import com.udacity.project4.locationreminders.CommonViewModel
 import com.udacity.project4.locationreminders.RemindersActivity
+import com.udacity.project4.locationreminders.data.ReminderDataSource
+import com.udacity.project4.locationreminders.data.local.FakeDataSource
 import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
-import com.udacity.project4.locationreminders.reminderslist.ReminderListFragmentDirections
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
 import org.hamcrest.CoreMatchers.not
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.module
 import org.koin.test.KoinTest
 import org.mockito.Mockito
 import org.mockito.Mockito.mock
@@ -37,8 +42,45 @@ import org.mockito.Mockito.mock
 class SaveReminderFragmentTest : KoinTest {
 
 
+    private lateinit var repository: ReminderDataSource
+    private lateinit var appContext: Application
+
     @get:Rule
     var activityTestRule: ActivityTestRule<RemindersActivity> = ActivityTestRule(RemindersActivity::class.java)
+
+
+    @Before
+    fun init() {
+        stopKoin()//stop the original app koin
+        appContext = ApplicationProvider.getApplicationContext()
+        val myModule = module {
+            single {
+                SaveReminderViewModel(
+                        appContext,
+                        repository
+                )
+            }
+
+            single {
+                CommonViewModel(
+                        repository
+                )
+
+            }
+
+        }
+        //declare a new koin module
+        startKoin {
+            modules(listOf(myModule))
+        }
+
+        repository = FakeDataSource()
+
+        //clear the data to start fresh
+        runBlocking {
+            repository.deleteAllReminders()
+        }
+    }
 
 
     @Test
@@ -55,5 +97,33 @@ class SaveReminderFragmentTest : KoinTest {
 
         onView(withText(R.string.err_enter_title)).inRoot(withDecorView(not(activityTestRule.activity.window.decorView))).check(matches(isDisplayed()))
     }
+
+    @Test
+    fun ifYouFillImportantField_NavigateToReminderListFragment(){
+
+        val reminderToSave = ReminderDataItem("TITLE1", "description", "LOCATION1", 1.0, 1.0, "id1")
+
+
+        val scenario = launchFragmentInContainer<SaveReminderFragment>(Bundle(), R.style.DifferentTheme)
+
+        val navController = mock(NavController::class.java)
+        scenario.onFragment {
+            Navigation.setViewNavController(it.view!!, navController)
+
+            //faking insert of value from SelectLocationFragment
+            it._viewModel.reminderSelectedLocationStr.value = reminderToSave.location
+            it._viewModel.latitude.value = reminderToSave.latitude
+            it._viewModel.longitude.value = reminderToSave.longitude
+        }
+
+        onView(withId(R.id.reminderTitle)).perform(replaceText(reminderToSave.title))
+        onView(withId(R.id.reminderDescription)).perform(replaceText(reminderToSave.description))
+
+        onView(withId(R.id.saveReminder)).perform(click())
+
+        Mockito.verify(navController).popBackStack()
+    }
+
+
 
 }
