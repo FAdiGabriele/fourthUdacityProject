028a3543f4b037d2a93c679ed741a6e7258f9451
Fabio A. di Gabriele <fabioa.digabriele@gmail.com>
2021-08-21 18:29:52 +0200

    updated PermissionTestManager

--- app/src/androidTest/java/com/udacity/project4/util/PermissionTestManager.kt
+++ app/src/androidTest/java/com/udacity/project4/util/PermissionTestManager.kt
@@ -1,5 +1,7 @@
 package com.udacity.project4.util
 
+import android.content.Context
+import android.location.LocationManager
 import android.os.Build
 import android.util.Log
 import androidx.test.platform.app.InstrumentationRegistry
@@ -9,6 +11,9 @@ import androidx.test.uiautomator.UiSelector
 import java.util.*
 
 //As reported here: https://www.kotlindevelopment.com/runtime-permissions-espresso-done-right/
+val runningO_MR1OrEarlier = Build.VERSION.SDK_INT <= Build.VERSION_CODES.O_MR1
+val runningP = Build.VERSION.SDK_INT == Build.VERSION_CODES.P
+val runningQOrLater = Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
 enum class PermissionOptions{
     NONE,
     ONLY_FOREGROUND,
@@ -23,31 +28,37 @@ fun grantPermissionsIfRequested(permissionOption: PermissionOptions) {
 
     if (Build.VERSION.SDK_INT >= 23) {
         val textButtonToClick = when{
-            Locale.getDefault().displayLanguage == Locale.ITALIAN.displayLanguage && Build.VERSION.SDK_INT >= 29 && permissionOption == PermissionOptions.FOREGROUND_AND_BACKGROUND  -> {
+            Locale.getDefault().displayLanguage == Locale.ITALIAN.displayLanguage && runningQOrLater && permissionOption == PermissionOptions.FOREGROUND_AND_BACKGROUND  -> {
                 "MENTRE USI L'APP"
             }
-            Locale.getDefault().displayLanguage == Locale.ITALIAN.displayLanguage && Build.VERSION.SDK_INT >= 29 && permissionOption == PermissionOptions.ONLY_FOREGROUND  -> {
+            Locale.getDefault().displayLanguage == Locale.ITALIAN.displayLanguage && runningQOrLater && permissionOption == PermissionOptions.ONLY_FOREGROUND  -> {
                 "SOLO QUESTA VOLTA"
             }
-            Locale.getDefault().displayLanguage == Locale.ITALIAN.displayLanguage && Build.VERSION.SDK_INT >= 29 && permissionOption == PermissionOptions.NONE -> {
+            Locale.getDefault().displayLanguage == Locale.ITALIAN.displayLanguage && runningQOrLater && permissionOption == PermissionOptions.NONE -> {
                 "RIFIUTA"
             }
-            Locale.getDefault().displayLanguage == Locale.ITALIAN.displayLanguage && Build.VERSION.SDK_INT < 29 && permissionOption != PermissionOptions.NONE-> {
+            Locale.getDefault().displayLanguage == Locale.ITALIAN.displayLanguage && runningP && permissionOption != PermissionOptions.ONLY_FOREGROUND-> {
+                "Consenti"
+            }
+            Locale.getDefault().displayLanguage == Locale.ITALIAN.displayLanguage && runningP && permissionOption != PermissionOptions.NONE-> {
+                "Nega"
+            }
+            Locale.getDefault().displayLanguage == Locale.ITALIAN.displayLanguage && runningO_MR1OrEarlier && permissionOption != PermissionOptions.NONE-> {
                 "CONSENTI"
             }
-            Locale.getDefault().displayLanguage == Locale.ITALIAN.displayLanguage && Build.VERSION.SDK_INT < 29 && permissionOption == PermissionOptions.NONE-> {
+            Locale.getDefault().displayLanguage == Locale.ITALIAN.displayLanguage && runningO_MR1OrEarlier && permissionOption == PermissionOptions.NONE-> {
                 "NEGA"
             }
-            Build.VERSION.SDK_INT >= 29 && permissionOption == PermissionOptions.FOREGROUND_AND_BACKGROUND  -> {
+            runningQOrLater && permissionOption == PermissionOptions.FOREGROUND_AND_BACKGROUND  -> {
                 "While using the app"
             }
-            Build.VERSION.SDK_INT >= 29 && permissionOption == PermissionOptions.ONLY_FOREGROUND  -> {
+            runningQOrLater && permissionOption == PermissionOptions.ONLY_FOREGROUND  -> {
                 "Only this time"
             }
-            Build.VERSION.SDK_INT >= 29 && permissionOption == PermissionOptions.NONE  -> {
+            runningQOrLater && permissionOption == PermissionOptions.NONE  -> {
                 "Deny"
             }
-            Build.VERSION.SDK_INT < 29 && permissionOption == PermissionOptions.NONE  -> {
+            runningO_MR1OrEarlier && permissionOption == PermissionOptions.NONE  -> {
                 "DENY"
             }
             else ->{
@@ -65,7 +76,7 @@ fun grantPermissionsIfRequested(permissionOption: PermissionOptions) {
     }
 }
 
-fun turnOnPositionIfRequested(locationsOption: LocationsOptions){
+fun turnOnPositionIfRequested(locationsOption: LocationsOptions) : Boolean{
     if (Build.VERSION.SDK_INT >= 23) {
         val textButtonToClick = when{
             Locale.getDefault().displayLanguage == Locale.ITALIAN.displayLanguage && locationsOption == LocationsOptions.KEEP_OFF -> {
@@ -82,10 +93,40 @@ fun turnOnPositionIfRequested(locationsOption: LocationsOptions){
         if (allowPermissions.exists()) {
             try {
                 allowPermissions.click()
+                return true
             } catch (e: UiObjectNotFoundException) {
                 Log.e("Test_TAG", "No permission dialog found.")
             }
         }
+        return false
     }
+    return true
+}
+
+fun resetAllPermissions(){
+    if(runningQOrLater) {
+        InstrumentationRegistry.getInstrumentation().uiAutomation
+            .executeShellCommand("pm revoke ${androidx.test.InstrumentationRegistry.getTargetContext().packageName} android.permission.ACCESS_BACKGROUND_LOCATION")
+    }
+
+    InstrumentationRegistry.getInstrumentation().uiAutomation.
+    executeShellCommand("pm revoke ${androidx.test.InstrumentationRegistry.getTargetContext().packageName} android.permission.ACCESS_FINE_LOCATION")
+    InstrumentationRegistry.getInstrumentation().uiAutomation.
+    executeShellCommand("pm revoke ${androidx.test.InstrumentationRegistry.getTargetContext().packageName} android.permission.ACCESS_COARSE_LOCATION")
+}
+
+
+fun checkIfLocationIsEnabled(context : Context): Boolean{
+    val lm = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+    var gps_enabled = false
+    var network_enabled = false
+
+    try {
+        gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    } catch (ex: Exception) {
+    }
+
+    return !(!gps_enabled && !network_enabled)
 }
 
